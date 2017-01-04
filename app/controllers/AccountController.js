f596cf97ae4336c10708d2fbf18138df782671a7
Ben-hur Santos Ott <Ben-hur Santos Ott>
2017-01-04 21:57:11 -0200

    feat(login)

--- app/controllers/AccountController.js
+++ app/controllers/AccountController.js
@@ -4,11 +4,103 @@ var chance = require('chance')();
 module.exports = function (app) {
     var controller = {};
 
-    var FacebookAuthToken = app.models.FacebookAuthToken;
+    var ERR_ACCOUNT_ALREADY_EXISTS = 'ERR_ACCOUNT_ALREADY_EXISTS';
+    var ERR_LOGIN_INVALID = 'ERR_LOGIN_INVALID';
+    var ERR_INTERNAL = 'ERR_INTERNAL';
+
+    var User = app.models.User;
+    var AuthToken = app.models.AuthToken;
+
+    controller.login = function(req, res) {
+        //TODO: crypt
+        var loginData = {
+            email: sanitize(req.body.email),
+            password: sanitize(req.body.password)
+        };
+
+        var token = chance.guid();
+
+        User.findOne(loginData)
+            .then(function(user) {
+                if(!user) {
+                    throw ERR_LOGIN_INVALID;
+                }
+
+                return AuthToken.findOne({
+                    email: loginData.email
+                });
+            })
+            .then(function(authToken) {
+                return !!authToken ?
+                    AuthToken.create({
+                        email: loginData.email,
+                        token: token
+                    })
+                    :
+                    AuthToken.update(
+                        { email: loginData.email },
+                        {
+                            $set: {
+                                email: loginData.email,
+                                token: token
+                            }
+                        }
+                    );
+            })
+            .then(function() {
+                res.status(200).json({
+                    token: token
+                });
+            })
+            .catch(function(err) {
+                if(err === ERR_LOGIN_INVALID) {
+                    res.status(400).json({
+                        code: ERR_LOGIN_INVALID
+                    });
+                }
+                else {
+                    res.status(500).json({
+                        code: ERR_INTERNAL
+                    });
+                }
+            });
+    };
+
+    controller.createAccount = function(req, res) {
+        // TODO: crypt pass.
+        var accountData = {
+            email: sanitize(req.body.email),
+            password: sanitize(req.body.password),
+            name: sanitize(req.body.name)
+        };
+        
+        User.findOne({email: accountData.email})
+            .then(function(user) {
+                if(!user) {
+                    return User.create(accountData);
+                }
+
+                throw ERR_ACCOUNT_ALREADY_EXISTS;
+            })
+            .then(function(){
+                res.status(200).end();
+            })
+            .catch(function(err) {
+                if(err === ERR_ACCOUNT_ALREADY_EXISTS) {
+                    res.status(400).json({
+                        code: ERR_ACCOUNT_ALREADY_EXISTS
+                    });
+                }
+                else {
+                    res.status(500).json({
+                        code: ERR_INTERNAL
+                    });
+                }
+            });
+    };
 
     controller.loginFacebook = function(req, res) {
         var loginData = {
-            email: sanitize(req.body.email),
             userId: sanitize(req.body.userId),
             accessToken: sanitize(req.body.accessToken)
         };
--- app/routes/AccountRoute.js
+++ app/routes/AccountRoute.js
@@ -1,6 +1,9 @@
 module.exports = function (app) {
     var controller = app.controllers.AccountController;
 
-    app.route('/account/login-facebook')
-        .post(controller.loginFacebook);
+    app.route('/account/login')
+        .post(controller.login);
+
+    app.route('/account/create')
+        .post(controller.createAccount);
 };
